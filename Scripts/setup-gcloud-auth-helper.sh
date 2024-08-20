e082d0dcad2925b6021ea2d09abed49c5c193334
David McRae <david.mcrae@buildertrend.com>
2024-08-20 10:09:55 -0500

    hardcode variables

--- Scripts/setup-gcloud-auth-helper.sh
+++ Scripts/setup-gcloud-auth-helper.sh
@@ -1,8 +1,8 @@
-VERSION=2.1.23
-OS=linux  # or "darwin" for OSX, "windows" for Windows.
-ARCH=amd64  # or "386" for 32-bit OSs
+# VERSION=2.1.23
+# OS=linux  # or "darwin" for OSX, "windows" for Windows.
+# ARCH=amd64  # or "386" for 32-bit OSs
 
-curl -fsSL "https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v${VERSION}/docker-credential-gcr_${OS}_${ARCH}-${VERSION}.tar.gz" \
+curl -fsSL "https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v2.1.23/docker-credential-gcr_linux_amd64-2.1.23.tar.gz" \
 | tar xz docker-credential-gcr \
 && chmod +x docker-credential-gcr && sudo mv docker-credential-gcr /usr/bin/
 
